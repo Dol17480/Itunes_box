ad6cbab6e9a2b0f43bf67f9217987d856cb9bb71
David O'Leary <david.oleary174@gmail.com>
2017-01-10 17:06:20 +0000

    selector added

--- client/src/components/TunesDetail.jsx
+++ client/src/components/TunesDetail.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
 
-var CountryDetail = function () {
+var TunesDetail = function (props) {
+  if (!props.song) {
+    return <h4>No song selected</h4>
+  }
   return (
-    <h3>
-      Country details to go here.
-    </h3>
+   <div className='tunes-detail'>
+   <a onClick={props.handleClick} href="">{props.character.name}</a>
+   <p>
+   <a href={props.character.house}>{props.character.house}</a>
+   </p>
+   <img src={props.character.image}/>
+   </div>
   );
 };
 
-module.exports = CountryDetail;
+module.exports = CharacterDetail;
+
--- client/src/components/TunesSelector.jsx
+++ client/src/components/TunesSelector.jsx
@@ -1,13 +1,30 @@
 var React = require('react');
 
-var CountrySelector = React.createClass({
+var TunesSelector = React.createClass({
+  getInitialState: function () {
+    return { selectedIndex: undefined };
+  },
   render: function () {
+    var options = this.props.songs.map(function (song, index) {
+
+      return (<option value={index} key={index}>{song.name}</option>);
+    });
     return (
-      <select id="countries">
-        <option>Country Selector</option>
+      <select 
+      id="songs" 
+      value={this.state.selectedIndex}
+      onChange={this.handleChange}
+      >
+        {options}
       </select>
     );
+  },
+  handleChange: function (event) {
+    var newIndex = parseInt(event.target.value);
+    this.setState({selectedIndex: newIndex});
+    var song = this.props.songs[newIndex];
+    this.props.selectSong(song);
   }
 });
 
-module.exports = CountrySelector;
+module.exports = TunesSelector;
